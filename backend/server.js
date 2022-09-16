2be379681b2effd5336bb72e0d8be3b55b113460
satria-kurniawan <satriakurniawan2001@gmail.com>
2022-09-16 18:07:42 +0800

    fix

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,6 @@ app.use("/api/users", require("./routes/userRoutes"))
 
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${port}`)
+)
